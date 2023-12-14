c8f937246a911ecae128b96159198766bb899bd9
Felipe Flausino Silva <112907664+Flausino3@users.noreply.github.com>
2023-12-14 09:32:46 -0300

    Add files via upload

---
+++ Herança/Quadrilateros/Circulo.java
@@ -0,0 +1,27 @@
+public class Circulo extends Forma{
+
+    private double raio;
+
+    public Circulo(double raio){
+        this.raio=raio;
+
+    }
+
+    public double getRaio(){
+        return raio;
+    }
+
+    public double getDiametro(){
+        return 2*raio;
+    }
+
+    public double getArea(){
+        return Math.PI * (Math.pow(raio,2));
+    }
+
+    public double getPerimetro(){
+        return 2* Math.PI * raio;
+    }
+
+
+}
